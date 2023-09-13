92113ca8a91cce85818301dad8b6dfebc2d0cc6a
Bbaranoski <breno.b.o.baranoski@gmail.com>
2023-09-13 15:58:35 -0300

    make pretier the code

--- src/dom.js
+++ src/dom.js
@@ -1,18 +1,28 @@
-const body = document.body
-const main = document.getElementById('main')
+const body = document.body;
+const main = document.getElementById("main");
 
-export function normal(person){
-    const home = document.getElementById('home')
-    const form = document.querySelector('form')
-    home.remove()
-    form.remove()
-    appendNormal(person)
+export function normal(person) {
+  const home = document.getElementById("home");
+  const form = document.querySelector("form");
+  home.remove();
+  form.remove();
+  appendNormal(person);
 }
 
-function appendNormal(person){
-    const div = document.createElement('div')
-    const name = document.createElement('h1')
-    name.textContent = person
-    div.appendChild(name)
-    main.appendChild(div)
+function appendNormal(person) {
+  const div = document.createElement("div");
+  const name = document.createElement("h1");
+  name.textContent = "Good day " + person;
+  div.appendChild(name);
+  main.appendChild(div);
 }
+
+export function especial() {
+  const home = document.getElementById("home");
+  const form = document.querySelector("form");
+  home.remove();
+  form.remove();
+  appendEspecial();
+}
+
+function appendEspecial() {}
--- src/form.js
+++ src/form.js
@@ -1,16 +1,20 @@
-import { normal } from './dom.js'
+import { normal, especial } from "./dom.js";
 
-const form = document.querySelector('form')
-const name = document.getElementById('name')
-const main = document.getElementById('main')
+const form = document.querySelector("form");
+const name = document.getElementById("name");
+const main = document.getElementById("main");
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    console.log(name.value)
-    const randomColor = 
-    Math.floor(Math.random()*16777215).toString(16);
-    main.style.backgroundColor = '#' + randomColor
-    main.style.transition = 'background-color 5s ease-out 0s'
-    normal(name.value)
-    name.value = ''
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const temp = name.value.toLowerCase();
+  main.style.transition = "background-color 5s ease-out 0s";
+  if (temp == "ana clara tondini siebert") {
+    main.style.backgroundColor = "#f472b6";
+    especial();
+  } else {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    main.style.backgroundColor = "#" + randomColor;
+    normal(name.value);
+  }
+  name.value = "";
+});
