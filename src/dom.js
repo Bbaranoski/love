2e76d02fef3eb4d100561c676fe6535b64da98d1
Bbaranoski <breno.b.o.baranoski@gmail.com>
2023-09-14 14:14:38 -0300

    almost done, just need the text

--- src/dom.js
+++ src/dom.js
@@ -1,30 +1,52 @@
 const body = document.body;
 const main = document.getElementById("main");
 
-const partOne = document.createElement('div')
-const partTwo = document.createElement('div')
-const partThree = document.createElement('div')
-partOne.setAttribute('id', 'partOne')
-partTwo.setAttribute('id', 'partTwo')
-partThree.setAttribute('id', 'partThree')
+const partOne = document.createElement("div");
+const partTwo = document.createElement("div");
+const partThree = document.createElement("div");
+partOne.setAttribute("id", "partOne");
+partTwo.setAttribute("id", "partTwo");
+partThree.setAttribute("id", "partThree");
 
-const buttonOne = document.createElement('button')
-const buttonTwo = document.createElement('button')
-buttonOne.classList.add('continuar')
-buttonTwo.classList.add('continuar')
-buttonOne.textContent = 'Continuar'
-buttonTwo.textContent = 'Continuar'
-partOne.appendChild(buttonOne)
-partTwo.appendChild(buttonTwo)
+const divOne = document.createElement("div");
+const divTwo = document.createElement("div");
+const divThree = document.createElement("div");
+partOne.appendChild(divOne);
+partTwo.appendChild(divTwo);
+partThree.appendChild(divThree);
 
-buttonOne.addEventListener('click', () => {
-    partOne.remove()
-    appendTwo()
-})
-buttonTwo.addEventListener('click', () => {
-    partTwo.remove()
-    appendThree()
-})
+const pOne = document.createElement("p");
+const pTwo = document.createElement("p");
+const pThree = document.createElement("p");
+divTwo.appendChild(pTwo);
+divThree.appendChild(pThree);
+pOne.textContent = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+pTwo.textContent = 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
+pThree.textContent = 'ccccccccccccccccccccccccccccccc'
+
+const buttonOne = document.createElement("button");
+const buttonTwo = document.createElement("button");
+buttonOne.classList.add("continuar");
+buttonTwo.classList.add("continuar");
+buttonOne.textContent = "Continuar";
+buttonTwo.textContent = "Continuar";
+partOne.appendChild(buttonOne);
+partTwo.appendChild(buttonTwo);
+
+buttonOne.addEventListener("click", () => {
+  pOne.style.color = "#ec4899";
+  setTimeout(() => {
+    partOne.remove();
+    appendTwo();
+  }, 2000);
+});
+buttonTwo.addEventListener("click", () => {
+  pTwo.style.color = '#ec4899'
+  setTimeout(() => {
+    partTwo.remove();
+    appendThree();
+  }, 2000);
+});
 
 export function normal(person) {
   const home = document.getElementById("home");
@@ -53,18 +75,30 @@ export function especial() {
 }
 
 function appendOne() {
-    main.appendChild(partOne)
+  main.appendChild(partOne);
+  setTimeout(() => {
+    partOne.style.backgroundColor = "#ec4899";
     setTimeout(() => {
-        partOne.style.backgroundColor = '#ec4899'
-    }, 2000);
+      divOne.appendChild(pOne);
+      setTimeout(() => {
+        pOne.style.color = "black";
+      }, 1000);
+    }, 3000);
+  }, 2000);
 }
 
-function appendTwo(){
-    main.appendChild(partTwo)
-    main.style.backgroundColor = '#f9a8d4'
+function appendTwo() {
+  main.appendChild(partTwo);
+  main.style.backgroundColor = "#f9a8d4";
+  setTimeout(() => {
+    pTwo.style.color = 'black'
+  }, 1000);
 }
 
-function appendThree(){
-  main.appendChild(partThree)
-  main.style.backgroundColor ='#b91c1c'
+function appendThree() {
+  main.appendChild(partThree);
+  main.style.backgroundColor = "#b91c1c";
+  setTimeout(() => {
+    pThree.style.color = 'black'
+  }, 1000);
 }
